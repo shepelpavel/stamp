c53151108a78bb26c3090027a259c923f804b25e
Shepel Pavel <shepelpavel@mail.ru>
2023-06-07 12:26:29 +0300

    fix: fix GHP path

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, './docs'),
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/stamp/' : '/',
 })
