96a71e868e558c1db1ecdaae613ebf7b689b396e
Shepel Pavel <shepelpavel@mail.ru>
2023-06-07 12:18:22 +0300

    fix: correct build path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
+const path = require('path')
+
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  outputDir: path.resolve(__dirname, './docs'),
+})
